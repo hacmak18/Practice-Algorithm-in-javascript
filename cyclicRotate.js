18afcf57060d3af2fd4dcb65c8eb08cf833cda8e
mayur <mak.99@outlook.com>
2021-10-09 10:44:34 +0530

    complete cyclicRotate.js

--- cyclicRotate.js
+++ cyclicRotate.js
@@ -1,25 +1,30 @@
-let numsToShit = 3
+let numsToShit = 6
 let a = [9, 8, 7, 6, 4, 2, 1, 3];
 let alen = a.length - 1;
 
-let temp = [];
-
-for (let j = 0; j < numsToShit; j++) {
-    temp[j] = a[alen--];
-}
-
-console.log(temp);
-
-function rotate(temp, numsToShit) {
-
+function rotate(temp, numsToShit, a) {
+    let ans;
     for (let k = 0; k < numsToShit; k++) {
-        for (let i = alen - 1; i >= 0; i--) {
-            a[i + 1] = a[i];
-        }
-        a[k]=temp[k];
+        ans = rotateByOne(a, k, temp);
     }
+    return ans;
 }
 
-rotate(temp, numsToShit);
+function rotateByOne(a, k, temp) {
+    for (let i = a.length - 1 - 1; i >= 0; i--) {
+        a[i + 1] = a[i];
+    }
+    a[0] = temp[k];
+    return a;
+}
 
-console.log(a)
+if (numsToShit >= a.length) {
+    console.log(`numsToShift should be less then ${a.length - 1}`);
+} else {
+    let temp = [];
+    for (let j = 0; j < numsToShit; j++) {
+        temp[j] = a[alen--];
+    }
+    let ans = rotate(temp, numsToShit, a);
+    console.log(ans);
+}
